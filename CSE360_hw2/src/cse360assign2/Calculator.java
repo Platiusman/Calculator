8354023067c43d96fef9dca95566542da5ef7461
Isaac <Isaac@LG-ISAAC-WALKER>
2019-02-25 19:28:51 -0700

    This is version 1 and the first commit of the assignment

---
+++ CSE360_hw2/src/cse360assign2/Calculator.java
@@ -0,0 +1,89 @@
+/**
+ * Author: Isaac Walker
+ * Class ID: 579
+ * Assignment: 2
+ */
+
+package cse360assign2;
+
+
+/**
+ * This class is a calculator that does addition, subtraction, 
+ * division, multiplication, returns a total integer and a history.
+ * @author Isaac Walker
+ * @version This is version 1. Has only the methods and JavaDoc
+ */
+public class Calculator
+{
+
+	private int total;
+	
+	
+	/**
+	 * Is the basic constructor of the calculator.
+	 */
+	public Calculator ()
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	
+	/**
+	 * Returns a total value.
+	 * @return is supposed to return the current total.
+	 */
+	public int getTotal ()
+	{
+		return 0;
+	}
+	
+	
+	/**
+	 * Does addition.
+	 * @param is the value to be added.
+	 */
+	public void add (int value)
+	{
+		
+	}
+	
+	
+	/**
+	 * Does subtraction.
+	 * @param value is the value to be subtracted.
+	 */
+	public void subtract (int value)
+	{
+		
+	}
+	
+	
+	/**
+	 * Does multiplication.
+	 * @param value the value to multiplied.
+	 */
+	public void multiply (int value)
+	{
+		
+	}
+	
+	
+	/**
+	 * Does division.
+	 * @param is the value to be divided.
+	 */
+	public void divide (int value)
+	{
+		
+	}
+	
+	
+	/**
+	 * Returns the history of actions as a string.
+	 * @return returns a string of the actions in a format.
+	 */
+	public String getHistory ()
+	{
+		return "";
+	}
+}
